50bee90672c182f8789b147dd01bddb3c2e2f787
InkyDemon <inkydmitrii@gmail.com>
2024-05-13 15:26:36 +0500

    New ES6 features

--- src/main/java/thebendy/cubecode/api/script/ScriptManager.java
+++ src/main/java/thebendy/cubecode/api/script/ScriptManager.java
@@ -17,6 +17,7 @@ public class ScriptManager {
 
     public static void evalCode(String code, int line, String sourceName, @Nullable Map<String, Object> properties) throws EcmaError {
         Context runContext = Context.enter();
+        runContext.setLanguageVersion(Context.VERSION_ES6);
         ScriptableObject scope = runContext.initStandardObjects();
 
         if (properties != null) {
